54b6acaba88238079a48d9f4f5d0e2ba9c1c830e
dscorza90 <dscorza@vicomtech.org>
2019-05-20 15:12:25 +0200

    ENH: basic clip function working

--- clip.py
+++ clip.py
@@ -1,74 +1,37 @@
 import vtk
-import numpy as np
-import SimpleITK as sitk
 
 
-def clip_volume_with_model(surface, volume, out=False):
+def clip_volume_with_surface_model(polydata, vtk_image, clip_outside_surface=False, fill_value=0):
     """
     Given a surface, it clips the volume providing the inside or outside part of the volume.
-    The surface must be closed.
-    :param surface:
-    :param volume:
-    :param out: if True, provide the external part of the volume (default is False)
+    It must be a closed mesh, and should be correctly mapped with the vtkVolume.
 
-    :return:
+    :param polydata:
+    :param vtk_image:
+    :param clip_outside_surface: if True, provide the external part of the volume (default is False)
+
+    :return: clipped_vtk_image
     """
+    # Convert model to stencil
+    polyToStencil = vtk.vtkPolyDataToImageStencil()
+    polyToStencil.SetInputData(polydata)
+    polyToStencil.SetOutputSpacing(vtk_image.GetSpacing())
+    polyToStencil.SetOutputOrigin(vtk_image.GetOrigin())
+    polyToStencil.SetOutputWholeExtent(vtk_image.GetExtent())
+    polyToStencil.Update()
+    # Apply the stencil to the volume
+    stencilToImage = vtk.vtkImageStencil()
+    stencilToImage.SetInputData(vtk_image)
+    stencilToImage.SetStencilData(polyToStencil.GetOutput())
+    if clip_outside_surface:
+        stencilToImage.ReverseStencilOff()
+    else:
+        stencilToImage.ReverseStencilOn()
+    stencilToImage.SetBackgroundValue(fill_value)
+    stencilToImage.Update()
 
-# Determine the transform between the box and the image IJK coordinate systems
+    # Update the volume with the stencil operation result
+    outputImageData = vtk.vtkImageData()
+    outputImageData.DeepCopy(stencilToImage.GetOutput())
 
-rasToModel = vtk.vtkMatrix4x4()
-if clippingModel.GetTransformNodeID() != None:
-    modelTransformNode = slicer.mrmlScene.GetNodeByID(clippingModel.GetTransformNodeID())
-    boxToRas = vtk.vtkMatrix4x4()
-    modelTransformNode.GetMatrixTransformToWorld(boxToRas)
-    rasToModel.DeepCopy(boxToRas)
-    rasToModel.Invert()
-
-ijkToRas = vtk.vtkMatrix4x4()
-inputVolume.GetIJKToRASMatrix(ijkToRas)
-
-ijkToModel = vtk.vtkMatrix4x4()
-vtk.vtkMatrix4x4.Multiply4x4(rasToModel, ijkToRas, ijkToModel)
-modelToIjkTransform = vtk.vtkTransform()
-modelToIjkTransform.SetMatrix(ijkToModel)
-modelToIjkTransform.Inverse()
-
-transformModelToIjk = vtk.vtkTransformPolyDataFilter()
-transformModelToIjk.SetTransform(modelToIjkTransform)
-transformModelToIjk.SetInputConnection(clippingModel.GetPolyDataConnection())
-
-# Use the stencil to fill the volume
-
-# Convert model to stencil
-polyToStencil = vtk.vtkPolyDataToImageStencil()
-polyToStencil.SetInputConnection(transformModelToIjk.GetOutputPort())
-polyToStencil.SetOutputSpacing(inputVolume.GetImageData().GetSpacing())
-polyToStencil.SetOutputOrigin(inputVolume.GetImageData().GetOrigin())
-polyToStencil.SetOutputWholeExtent(inputVolume.GetImageData().GetExtent())
-
-# Apply the stencil to the volume
-stencilToImage = vtk.vtkImageStencil()
-stencilToImage.SetInputConnection(inputVolume.GetImageDataConnection())
-stencilToImage.SetStencilConnection(polyToStencil.GetOutputPort())
-if clipOutsideSurface:
-    stencilToImage.ReverseStencilOff()
-else:
-    stencilToImage.ReverseStencilOn()
-stencilToImage.SetBackgroundValue(fillValue)
-stencilToImage.Update()
-
-# Update the volume with the stencil operation result
-outputImageData = vtk.vtkImageData()
-outputImageData.DeepCopy(stencilToImage.GetOutput())
-
-outputVolume.SetAndObserveImageData(outputImageData);
-outputVolume.SetIJKToRASMatrix(ijkToRas)
-
-# Add a default display node to output volume node if it does not exist yet
-if not outputVolume.GetDisplayNode:
-    displayNode = slicer.vtkMRMLScalarVolumeDisplayNode()
-    displayNode.SetAndObserveColorNodeID("vtkMRMLColorTableNodeGrey")
-    slicer.mrmlScene.AddNode(displayNode)
-    outputVolume.SetAndObserveDisplayNodeID(displayNode.GetID())
-
-return True
+    return outputImageData
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
-import SimpleITK as sitk
 import vtk
+import SimpleITK as sitk
+import clip
 import transforms as trsf
 import numpy as np
 
@@ -10,7 +11,7 @@ scene_path = os.path.join(data_path, 'scene')
 model_paths = os.path.join(scene_path, 'models')
 surfaces = ['rh_pial.vtk', 'rh_white.vtk', 'lh_pial.vtk', 'lh_white.vtk']
 image_path = os.path.join(scene_path, 'ref.nii.gz')
-clipOutsideSurface = False
+clipOutsideSurface = True
 fillValue = 0
 print(data_path)
 
@@ -38,36 +39,24 @@ surf = surf_reader.GetOutput()
 ras2ijk = trsf.get_ras2ijk(image)
 surf_at_ijk = trsf.transform_poly_data(surf, ras2ijk)
 
-# Convert model to stencil
-polyToStencil = vtk.vtkPolyDataToImageStencil()
-polyToStencil.SetInputData(surf_at_ijk)
-polyToStencil.SetOutputSpacing(vtkIm.GetSpacing())
-polyToStencil.SetOutputOrigin(vtkIm.GetOrigin())
-polyToStencil.SetOutputWholeExtent(vtkIm.GetExtent())
+outputImageData = clip.clip_volume_with_surface_model(surf_at_ijk, vtkIm)
 
-writer = vtk.vtkImageWriter()
-writer.SetInputData(polyToStencil.GetOutput())
-# writer.SetFileName(os.path.join(data_path, 'poly2stencil.mha'))
+outputImageData.SetOrigin(vtkIm.GetOrigin())
+outputImageData.SetSpacing(vtkIm.GetSpacing())
+
+writer = vtk.vtkNIFTIImageWriter()
+writer.SetInputData(outputImageData)
+writer.SetFileName(os.path.join(data_path, 'resultsvtk.nii.gz'))
 writer.Update()
 
-# Apply the stencil to the volume
-stencilToImage = vtk.vtkImageStencil()
-stencilToImage.SetInputData(vtkIm)
-stencilToImage.SetStencilConnection(polyToStencil.GetOutputPort())
-if clipOutsideSurface:
-    stencilToImage.ReverseStencilOff()
-else:
-    stencilToImage.ReverseStencilOn()
-stencilToImage.SetBackgroundValue(fillValue)
-stencilToImage.Update()
+out_image = sitk.ReadImage(os.path.join(data_path, 'resultsvtk.nii.gz'))
+out_image.SetSpacing(image.GetSpacing())
+out_image.SetDirection(image_lps[:3, :3].ravel())
+out_image.SetOrigin(image_lps[:3, 3])
 
-clipedImg = stencilToImage.GetOutput()
-
-writer = vtk.vtkImageWriter()
-writer.SetInputData(clipedImg)
-writer.SetFileName(os.path.join(data_path, 'results.mha'))
-writer.Update()
-
+sitk.WriteImage(out_image, os.path.join(data_path, 'results.mha'))
+meta_output = sitk.ReadImage(os.path.join(data_path, 'results.mha'))
+sitk.WriteImage(out_image, os.path.join(data_path, 'nifti_result.nii.gz'), True)
 
 print('ciao')
 
