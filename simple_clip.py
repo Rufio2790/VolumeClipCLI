4899e6720452921e784695d289b44fb25fe7f17a
dscorza90 <dscorza@vicomtech.org>
2019-05-20 17:12:25 +0200

    ENH, TST: added testing, added possibility to pass a matrix to be applied before the clipping

--- simple_clip.py
+++ simple_clip.py
@@ -7,6 +7,8 @@ import sys
 import logging
 import SimpleITK as sitk
 import argparse
+import numpy as np
+
 import lib.clip as clip
 import lib.read_and_write as rw
 import lib.transforms as trsf
@@ -20,6 +22,10 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 root.addHandler(handler)
 
+ras2lps = np.identity(4)
+ras2lps[0, 0] = -1
+ras2lps[1, 1] = -1
+
 def main():
 
     parser = argparse.ArgumentParser()
@@ -30,18 +36,40 @@ def main():
     parser.add_argument('--inside', type=bool, help='True: Keep volume INSIDE (True) or OUTSIDE (False) the model; '
                                                'default is True ', required=True)
     parser.add_argument('-o', '--output', type=str, help='Output path', required=True)
+    parser.add_argument('-t', '--transform', type=str, help='Transform to apply to the surfaces before clipping '
+                                                            '(optional')
 
     logging.info("Parsing arguments...")
     args = parser.parse_args()
+    load_and_process(args.image, args.surf, args.output, inside_bool=args.inside, transform_path=args.transform)
 
+
+
+def load_and_process(image_path, surface_path, output_path, inside_bool=False, transform_path=None):
     logging.info("Reading images and surfaces...")
     # loading images as itk and vtk volume
-    sitk_im, vtk_im = rw.load_images(args.image)
+    sitk_im, vtk_im = rw.load_images(image_path)
     # forcing spacing to 1 for vtk image
     vtk_im.SetSpacing([1., 1., 1.])
 
     # loading surface
-    surf = rw.read_vtk_poly_data(args.surf)
+    surf = rw.read_vtk_poly_data(surface_path)
+
+    if transform_path:
+        t = sitk.ReadTransform(transform_path)
+        t = np.array(t.GetParameters())
+        c2r = np.identity(4)
+        c2r[:3, :3] = t[:9].reshape(3, 3)
+        c2r[:3, 3] = t[9:]
+        print('Reading centered2ref')
+        print(c2r)
+        c2r = np.dot(np.linalg.inv(c2r), ras2lps)
+        c2r = np.dot(ras2lps, c2r)
+        print('Transforming to RAS')
+        print(c2r)
+        surf = trsf.transform_poly_data(surf, c2r)
+
+        rw.write_vtk_poly_data(surf, os.path.join(os.path.dirname(output_path), "trsf_surf.vtk"))
 
     # getting image transformation matrix
     ras2ijk = trsf.get_ras2ijk(sitk_im)
@@ -49,12 +77,12 @@ def main():
     # transforming the surface to match with image at IJK -> Getting from RAS because surfaces are in RAS
     surf_at_ijk = trsf.transform_poly_data(surf, ras2ijk)
 
-    logging.info("Performing Clipping...")
+    logging.info("Clipping the image...")
     # clipping the image
-    out_vtk_image = clip.clip_volume_with_surface_model(surf_at_ijk, vtk_im, clip_outside_surface=args.inside)
+    out_vtk_image = clip.clip_volume_with_surface_model(surf_at_ijk, vtk_im, clip_outside_surface=inside_bool)
 
     # writing temporary nifty image without spatial information
-    temp_file_path = os.path.join(os.path.dirname(args.output), "temp.nii.gz")
+    temp_file_path = os.path.join(os.path.dirname(output_path), "temp.nii.gz")
     rw.write_vtk_nifti_image(out_vtk_image, temp_file_path)
 
     # reading the same image with itk
@@ -67,7 +95,7 @@ def main():
 
     logging.info("Saving image...")
     # writing the clipped image
-    sitk.WriteImage(out_image, args.output)
+    sitk.WriteImage(out_image, output_path)
 
     # removing temporary image
     os.remove(temp_file_path)
---
+++ tests/test_simple_clip.py
@@ -0,0 +1,24 @@
+import unittest
+import os
+from tests import data_path
+from simple_clip import load_and_process
+
+class TestSimpleClip(unittest.TestCase):
+
+    def setUp(self):
+        self.image_path = os.path.join(data_path, 'scene', 'ref.nii.gz')
+        self.surf_path = os.path.join(data_path, 'scene', 'models', 'lh_pial.vtk')
+        self.surf_not_centered_path = os.path.join(data_path, 'scene', 'models', 'lh_pial_no_centered.vtk')
+        self.transform_path = os.path.join(data_path, 'auxiliary', 'centered2ref.tfm')
+
+    def test_clipping(self):
+        load_and_process(self.image_path, self.surf_path, os.path.join(data_path, 'test_result.mha'),
+                         inside_bool=True)
+
+    def test_clipping_applying_matrix(self):
+        load_and_process(self.image_path, self.surf_not_centered_path, os.path.join(data_path, 'test_result.mha'),
+                         inside_bool=True, transform_path=self.transform_path)
+
+
+if __name__ == '__main__':
+    unittest.main()
